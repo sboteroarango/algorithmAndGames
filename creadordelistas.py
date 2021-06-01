f1b7f463eacf2def29e124487ce4f2f3199c1fbb
Simón Botero <sboteroarango@gmail.com>
2021-06-01 17:25:48 -0500

    Update creadordelistas.py

--- creadordelistas.py
+++ creadordelistas.py
@@ -10,65 +10,8 @@ for i in range(150):
     listaideal.append(x)
 print(listaideal)
 
-#JUEGO DE DADOS
 
-import random
 
-nombre1=str(input("ingrese su nombre"))
-nombre2=str(input("ingrese su nombre"))
-print("gana el que acierte primero el objetivo de la suma de los 2 dados primero")
-objetivo=random.randint(1,12)
-print("el objetivo de la suma es: ",objetivo)
-points1=0
-points2=0
-invitacion=input("Quiere jugar(si/no)")
-while (points1<1 or points2<1) and invitacion=="si":
-    dado1_1=random.randint(1,6)
-    dado1_2=random.randint(1,6)
-    dado2_1=random.randint(1,6)
-    dado2_2=random.randint(1,6)
-
-    print("los resultado de ",nombre1," son iguales a ",dado1_1,"y ",dado1_2)
-    print("los resultado de ",nombre2," son iguales a ",dado2_1,"y ",dado2_2)
-
-    suma1=dado1_1+dado1_2
-    suma2=dado2_1+dado2_2
-
-    if suma1==objetivo:
-        points1+=1
-    else:
-        print("fallaste,",nombre1, "tu suma es ",suma1)
-        pass
-    if suma2==objetivo:
-        points2+=1
-    else:
-        print("fallaste,",nombre2, "tu suma es ", suma2)
-        pass
-    pregunta= input("Quieres continuar (si/no)")
-    if pregunta=="si":
-        continue
-    if pregunta=="no":
-        break
-    
-if points1==1:
-    print("el ganador es ",nombre1)
-elif points2==1:
-    print("el ganador es", nombre2)
-else:
-    print("juego terminado")
-
-#ENCONTRADOR DE AÑOS BISIESTOS
-
-def esbisisiesto(año):
-    if (año%4)==0:
-        if (año%100)==0:
-            if (año%400)==0:
-                return año," es bisiesto"
-            return año," no es bisiesto"
-        
-        return año," es bisiesto"
-    return año, " no es bisiesto"
-aso=print(esbisisiesto(4000))
 
 
 #VALIDADOR DE CONTRASEÑAS
