8b5153e8de476ff3a0d34b7f7f0254419e6a8509
Simón Botero <sboteroarango@gmail.com>
2021-06-27 12:57:11 -0500

    Create fibonacci.java

---
+++ fibonacci.java
@@ -0,0 +1,14 @@
+double termino=0;
+double termino1=1;
+System.out.println(termino);
+System.out.println(termino1);
+double reemplazo;
+for(int i=0;i<=22;i++)
+{
+  reemplazo=termino1;
+  termino1=termino1+termino;
+  termino=reemplazo;
+  System.out.println(termino1);
+}
+
+                
