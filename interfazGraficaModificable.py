9d093b6d27cdc469c8447c1e33dd3ddd1c83603a
Simón Botero <sboteroarango@gmail.com>
2021-07-04 08:44:15 -0500

    Create interfazGraficaModificable.py

---
+++ interfazGraficaModificable.py
@@ -0,0 +1,15 @@
+print('Ingrese el largo y el ancho de la interfaz que desea como 2 numeros enteros separados por espacio :')
+datos = input().split()
+N,M = int(datos[0]), int(datos[1])
+simbolo = ''
+
+for i in range(int((N-1)/2)):
+    simbolo = '.|.' * (2*(i)+1)
+    print('-'*(int((M-len(simbolo))/2))+simbolo+'-'*(int((M-len(simbolo))/2)))
+
+print('-'*(int((M-7)/2))+'WELCOME'+'-'*(int((M-7)/2)))
+
+for i in range(int((N-1)/2)):
+    print('-'*(int((M-len(simbolo))/2))+simbolo+'-'*(int((M-len(simbolo))/2)))
+    simbolo = simbolo.replace('.|.'*2,'',1)
+    
