f524e4b55245d38e6350c5b181ddddcd9befcae7
Simón Botero <sboteroarango@gmail.com>
2021-07-04 08:49:22 -0500

    Update interfazGraficaModificable.py

--- interfazGraficaModificable.py
+++ interfazGraficaModificable.py
@@ -1,4 +1,4 @@
-print('Ingrese el largo y el ancho de la interfaz que desea como 2 numeros enteros separados por espacio :')
+print('Ingrese el largo y el ancho de la interfaz que desea como 2 numeros impares separados por espacio, donde el segundo es el primero por 3 ej:(7 21) :')
 datos = input().split()
 N,M = int(datos[0]), int(datos[1])
 simbolo = ''
