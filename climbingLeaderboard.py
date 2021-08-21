cfb0679b7b651a3f607fa8db4802469fd79ef684
Simón Botero <sboteroarango@gmail.com>
2021-08-21 13:26:30 -0500

    Update climbingLeaderboard.py

--- climbingLeaderboard.py
+++ climbingLeaderboard.py
@@ -1,1 +1,23 @@
 
+def climbingLeaderboard(ranked, player):
+    
+    rankedSinDuplicados = list(dict.fromkeys(ranked))
+    clasificaciones = []
+    for play in player:
+        
+        if play in rankedSinDuplicados:
+            clasificaciones.append(rankedSinDuplicados.index(play)+1)
+        if play not in rankedSinDuplicados:
+            if play< ranked[-1]:
+                clasificaciones.append(len(rankedSinDuplicados)+1)
+            elif play> ranked[0]:
+                clasificaciones.append(1)
+            else:
+                
+                for i in range(len(rankedSinDuplicados)):
+                    if (rankedSinDuplicados[i]>play) and (rankedSinDuplicados[i+1]<play):
+                        clasificaciones.append(i+2) 
+    
+    return clasificaciones
+
+
